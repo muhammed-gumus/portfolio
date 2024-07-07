d1b4598c94b644ac42e783e55e98e6e8f75cc31a
muhammed-gumus <mr.silver.mg@gmail.com>
2024-07-07 21:49:33 +0300

    ğ

--- new-portfolio/backend/models.py
+++
@@ -1,9 +0,0 @@
-# from pydantic import BaseModel, Field
-# from typing import Optional
-
-# class BlogPost(BaseModel):
-#     id: Optional[str] = Field(alias="_id")
-#     title: str
-#     date: str
-#     description: str
-#     slug: str
