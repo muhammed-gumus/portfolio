93c3a6e266c6470ab97e80aa4ff75dd313155cb4
muhammed-gumus <mr.silver.mg@gmail.com>
2024-11-08 10:53:53 +0300

    added sitemap

---
+++ new-portfolio/next-sitemap.config.js
@@ -0,0 +1,8 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://muhammedgumus.com', // Sitenizin ana URL'si (https:// ile başlayan tam adres)
+    generateRobotsTxt: true, // robots.txt dosyası oluşturur
+    changefreq: 'daily', // Sayfaların güncellenme sıklığı
+    priority: 0.7, // Sayfa öncelik değeri
+  };
+  
