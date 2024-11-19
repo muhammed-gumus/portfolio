7bc8c550ccec263aebf0d9298887572e8539c5f9
muhammed-gumus <mr.silver.mg@gmail.com>
2024-11-19 15:40:35 +0300

    update cv

--- new-portfolio/components/Navbar/index.tsx
+++ new-portfolio/components/Navbar/index.tsx
@@ -69,7 +69,7 @@ export default function Navbar() {
         </Link>
         <div className="flex flex-row gap-8 items-center">
           <a
-            href="/MuhammedGumusResumeCV.pdf"
+            href="/CvMuhammedGumus.pdf"
             target="_blank"
             rel="noopener noreferrer"
             className={`lg ${
