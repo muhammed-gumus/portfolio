53849c6255959ba02a1b40fe39398c143f068861
muhammed-gumus <mr.silver.mg@gmail.com>
2025-09-13 00:54:57 +0300

    fix: adjust padding and comment out horizontal rules for improved layout clarity

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,14 @@ import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-full items-center justify-center py-6 px-8 md:px-24 bg-white dark:bg-black">
+    <main className="flex flex-col w-full items-center justify-center py-6 px-2 md:px-24 bg-white dark:bg-black">
       <Banner />
       <Experience />
-      <hr className="border-gray-700 my-6 w-full" />
+      {/* <hr className="border-gray-700 my-6 w-full" /> */}
       <Projects />
-      <hr className="border-gray-700 my-6 w-full" />
+      {/* <hr className="border-gray-700 my-6 w-full" /> */}
       <Education />
-      <hr className="border-gray-700 my-6 w-full" />
+      {/* <hr className="border-gray-700 my-6 w-full" /> */}
       <Contact />
     </main>
   );
