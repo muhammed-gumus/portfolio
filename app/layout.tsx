12e8fcfd76a089b2b8e5f64eee7a924fbc570489
muhammed-gumus <mr.silver.mg@gmail.com>
2025-12-03 16:27:27 +0300

    feat: Integrate Vercel Speed Insights for performance monitoring.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -41,6 +42,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
